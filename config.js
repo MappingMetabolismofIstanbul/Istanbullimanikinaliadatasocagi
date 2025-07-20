8952284da1bb1a979a70bf3796f3b1e2dfbee1c1
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-20 17:56:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'İstanbul Limanı Galata Rıhtımı / Port of İstanbul',
+    title: 'İstanbul Limanı Kınalıada Taşocağı / Kınalıada Quarry of the Istanbul Port',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
